dfdb3d1ef0b2dbb529fc1d8f8e402ca214f1eed3
Md. Golam Mostofa <golammostofa10001@gmail.com>
2024-02-09 20:55:11 +0600

    email completed

--- force-app/main/default/lwc/temp1ForgotPassComp/temp1ForgotPassComp.js
+++ force-app/main/default/lwc/temp1ForgotPassComp/temp1ForgotPassComp.js
@@ -1,16 +1,25 @@
 import logo from '@salesforce/resourceUrl/Tforce_logo';
-
+import forgotPassword from'@salesforce/apex/ForgotPassController.customForgotPassword'
 import { LightningElement, track } from 'lwc';
 
 export default class Temp1ForgotPassComp extends LightningElement {
     @track company_logo = logo;
-    username = '';
-    errorMessage = 'an error';
+    @track username = '';
+    errorMessage = ''; // I will show occure an error
     handleUsername(event){
         this.username = event.target.value;
     }
 
-    handleLogin(){
-
+    handlesend(){
+        console.log(this.username);
+        forgotPassword({username:this.username})
+        .then(response => {
+            this.errorMessage = response
+            console.log('response', response)
+        })
+        .catch(error => {
+            this.errorMessage = error.body
+            console.log('error', error)
+        })
     }
 }
